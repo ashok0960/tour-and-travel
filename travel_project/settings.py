fb673161d6ec4eaf79ef45fbae2b543db5ba2717
ashok0960 <ashokkumarkari5@gmail.com>
2026-05-15 22:38:34 +0545

    Fix Vercel deployment

--- travel_project/settings.py
+++ travel_project/settings.py
@@ -10,12 +10,25 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config(
+    'SECRET_KEY',
+    default='django-temporary-key'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config(
+    'DEBUG',
+    default=False,
+    cast=bool
+)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend', '0.0.0.0']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'backend',
+    '0.0.0.0',
+    '.vercel.app'
+]
 
 # Application definition
 INSTALLED_APPS = [
